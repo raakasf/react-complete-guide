b165e6d0e861a17bb2d55035b74ae1f1a115845a
Rakesh Sehgal <rakesh.sehgal@viasat.com>
2021-04-21 09:45:09 -0400

    cloned

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component }  from 'react';
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  const para = document.createElement('p');
+  para.textContent = 'This is also visible';
+  document.getElementById('root').append(para);
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './ExpenseItem.css'
+
+function ExpenseItem() {
+    const expenseDate = new Date(2021, 4, 20);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;
+
+    return (<div className="expense-item">
+        <div>{expenseDate.toISOString()}</div>
+        <div className="expense-item__description">
+            <h2>{expenseTitle}</h2>
+            <div className="expense-item__price">${expenseAmount}</div>
+        </div>
+    </div>);
+}
+
+export default ExpenseItem;
