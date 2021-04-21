b624a3e75a0e8f691a303ed81f79ecd01fb29992
Rakesh Sehgal <rakesh.sehgal@viasat.com>
2021-04-21 10:33:18 -0400

    pass props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -6,7 +6,7 @@ import ExpenseDate from './ExpenseDate';
 function ExpenseItem(props) {
   return (<div className="expense-item">
     <div>
-      <ExpenseDate/>
+      <ExpenseDate date={props.date}/>
     </div>
     <div className="expense-item__description">
       <h2>{props.title}</h2>
