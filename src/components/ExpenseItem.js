85abcc914fee214c97d9102fa15befbfc3ed893b
Rakesh Sehgal <rakesh.sehgal@viasat.com>
2021-04-21 10:22:22 -0400

    Refactor

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,13 +2,21 @@ import React from 'react';
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
-    return (<div className="expense-item">
-        <div>{props.date.toLocaleString('en-US', {month: 'long'})}</div>
-        <div className="expense-item__description">
-            <h2>{props.title}</h2>
-            <div className="expense-item__price">${props.amount}</div>
-        </div>
-    </div>);
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const year = prop.date.getFullYear();
+
+  return (<div className="expense-item">
+    <div>
+      <div>{month}</div>
+      <div>{year}</div>
+      <div>{day}</div>
+    </div>
+    <div className="expense-item__description">
+      <h2>{props.title}</h2>
+      <div className="expense-item__price">${props.amount}</div>
+    </div>
+  </div>);
 }
 
 export default ExpenseItem;
