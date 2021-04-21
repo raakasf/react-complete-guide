6537249f03088d2fca6427af707402c8e80b4446
Rakesh Sehgal <rakesh.sehgal@viasat.com>
2021-04-21 10:27:34 -0400

    Refactor date to new comp

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import './ExpenseItem.css'
+import './ExpenseDate';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString('en-US', { month: 'long' });
-  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
-  const year = prop.date.getFullYear();
-
   return (<div className="expense-item">
     <div>
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
+      <ExpenseDate/>
     </div>
     <div className="expense-item__description">
       <h2>{props.title}</h2>
