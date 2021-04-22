98c308dba7bc180843f6156d5cffbe7ec80eead2
Rakesh Sehgal <rakesh.sehgal@viasat.com>
2021-04-22 11:50:39 -0400

    Add submit handler

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -10,26 +10,44 @@ const ExpenseForm = (props) => {
   });
 
   const titleChangeHandler = (event) => {
-    setUserInput({
-      ...userInput,
-      enteredTitle: event.target.value
+    setUserInput((prevState) => {
+      return {
+        ...userInput,
+        enteredTitle: event.target.value
+      }
     });
   };
   const amountChangeHandler = (event) => {
-    setUserInput({
-      ...userInput,
-      enteredAmount: event.target.value,
+    setUserInput((prevState) => {
+      return {
+        ...userInput,
+        enteredAmount: event.target.value,
+      }
     })
   };
   const dateChangeHandler = (event) => {
-    setUserInput({
-      ...userInput,
-      enteredDate: event.target.value,
+    setUserInput((prevState) => {
+      return {
+        ...userInput,
+        enteredDate: event.target.value,
+      }
     })
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: userInput.enteredTitle,
+      amount: userInput.enteredAmount,
+      date: new Date(userInput.enteredDate),
+    }
+    console.log(expenseData);
+    return expenseData;
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
@@ -37,7 +55,7 @@ const ExpenseForm = (props) => {
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' min="0.01" max="0.01" onChange={amountChangeHandler} />
+          <input type='number' min="0.01" step="0.01" onChange={amountChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
