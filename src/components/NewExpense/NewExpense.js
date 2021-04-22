8e8e38d63a6d37d5db343f720dc20e4e3d505b24
Rakesh Sehgal <rakesh.sehgal@viasat.com>
2021-04-22 10:45:27 -0400

    Add new expenses

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ExpenseForm from "./ExpenseForm";
+import './NewExpense.css';
+
+const NewExpenses = (props) => {
+  return (
+    <div className='new-expense'>
+      <ExpenseForm />
+    </div>
+  );
+}
+
+export default NewExpenses;
