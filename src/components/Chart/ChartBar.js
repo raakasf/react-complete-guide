9e92b852743f1ba8b55d4c2c5baa17fedc65a27c
Rakesh Sehgal <rakesh.sehgal@viasat.com>
2021-05-04 11:14:23 -0400

    ERror

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,27 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import './ChartBar.css';
+import "./ChartBar.css";
 
 const ChartBar = (props) => {
-  let barFillHeight = '0%';
+  let barFillHeight = "0%";
 
   if (props.max > 0) {
-    barFillHeight = Math.round((props.value / props.maxValue) * 100) + '%';
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
   }
 
-  return <div className='chart-bar'>
-    <div className='chart-bar__inner'>
-      <div className='chart-bar__fill'
-        style={{ height: barFillHeight }}>
+  return (
+    <div className="chart-bar">
+      <div className="chart-bar__inner">
+        <div
+          className="chart-bar__fill"
+          style={{ height: barFillHeight }}
+        ></div>
       </div>
+      <div className="chart-bar__label"></div>
     </div>
-    <div className='chart-bar__label'>
-    </div>
-
-    {
-
-    }
-  </div>
+  );
 };
 
 export default ChartBar;
