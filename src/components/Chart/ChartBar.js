7e7dac02d69272ccbbfd7abd9ff1bc16a5acb3f0
Rakesh Sehgal <rakesh.sehgal@viasat.com>
2021-05-04 14:37:50 -0400

    format

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -12,12 +12,9 @@ const ChartBar = (props) => {
   return (
     <div className="chart-bar">
       <div className="chart-bar__inner">
-        <div
-          className="chart-bar__fill"
-          style={{ height: barFillHeight }}
-        ></div>
+        <div className="chart-bar__fill" style={{ height: barFillHeight }} />
       </div>
-      <div className="chart-bar__label"></div>
+      <div className="chart-bar__label" />
     </div>
   );
 };
