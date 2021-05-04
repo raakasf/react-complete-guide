157239589e2aa0ef418412cfb45bae9a9bbbbbf1
Rakesh Sehgal <rakesh.sehgal@viasat.com>
2021-05-04 11:50:06 -0400

    Typo

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -3,7 +3,7 @@ import React from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./ExpensesList.css";
 
-const Expenses = (props) => {
+const ExpensesList = (props) => {
   if (props.items.length === 0) {
     return <h2 className="expenses-list__fallback">Found no expenses.</h2>;
   }
@@ -22,4 +22,4 @@ const Expenses = (props) => {
   );
 };
 
-export default Expenses;
+export default ExpensesList;
